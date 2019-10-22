1e1c3f07e346c0ccce1283f62ca4dd920f059b90
张新罡 <zhangxingang@maoyan.com>
2019-10-22 15:34:13 +0800

    update

---
+++ src/com.leetcode.zh3ng.solutions/PlusOne66.java
@@ -0,0 +1,16 @@
+package com.leetcode.zh3ng.solutions;
+
+/*
+ * created by zhangxingang on 2019/09/16
+ */
+public class PlusOne66 {
+    public static int[] plusOne(int[] digits) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] num = new int[]{1,2,3,9};
+
+        System.out.println(plusOne(num));
+    }
+}
