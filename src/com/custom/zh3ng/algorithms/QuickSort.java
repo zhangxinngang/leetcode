62585787b0a471c88855f1b8c4680c3184d3822c
张新罡 <zhangxingang@maoyan.com>
2019-11-22 15:37:19 +0800

    没事熟悉一下算法代码，写了个快排

---
+++ src/com/custom/zh3ng/algorithms/QuickSort.java
@@ -0,0 +1,51 @@
+package com.custom.zh3ng.algorithms;
+
+/*
+ * created by zhangxingang on 2019/11/22
+ */
+public class QuickSort {
+    public void sort(int[] a,int low,int high) {
+        int start = low;
+        int end = high;
+        int key = a[start];
+
+        while(start < end){
+            while (start < end && key <= a[end]){
+                end--;
+            }
+            if (key > a[end]){
+                int temp = a[end];
+                a[end] = a[start];
+                a[start] = temp;
+            }
+            while (start < end && key >= a[start]){
+                start++;
+            }
+            if (key < a[start]){
+                int temp = a[start];
+                a[start] = a[end];
+                a[end] = temp;
+            }
+        }
+
+        if (low < start){
+            sort(a,low,start-1);
+        }
+        if (high > end){
+            sort(a,end+1,high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{2,3,1,2,34,14,23,78,112,11,11};
+
+        QuickSort quickSort = new QuickSort();
+        quickSort.sort(a,0,a.length-1);
+
+        for (int i = 0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+
+    }
+
+}
