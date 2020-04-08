436823932ecb5f0e8b2459a26c568e6f28793388
张新罡 <zhangxingang@maoyan.com>
2020-04-08 16:21:40 +0800

    add a solution MiddleoftheLinkedList For30Days

---
+++ src/com.leetcode.zh3ng.solutions/MiddleoftheLinkedListFor30Days.java
@@ -0,0 +1,70 @@
+package com.leetcode.zh3ng.solutions;
+
+import model.ListNode;
+
+/**
+ * created by zhangxingang on 2020/04/08
+ *
+ * Question:
+ *
+ * Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+ *
+ * If there are two middle nodes, return the second middle node.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: [1,2,3,4,5]
+ * Output: Node 3 from this list (Serialization: [3,4,5])
+ * The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+ * Note that we returned a ListNode object ans, such that:
+ * ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+ * Example 2:
+ *
+ * Input: [1,2,3,4,5,6]
+ * Output: Node 4 from this list (Serialization: [4,5,6])
+ * Since the list has two middle nodes with values 3 and 4, we return the second one.
+ *
+ *
+ * Note:
+ *
+ * The number of nodes in the given list will be between 1 and 100.
+ *
+ */
+public class MiddleoftheLinkedListFor30Days {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == null){
+                return slow;
+            }
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        n1.next = n2;
+        ListNode n3 = new ListNode(3);
+        n2.next = n3;
+        ListNode n4 = new ListNode(4);
+        n3.next = n4;
+        ListNode n5 = new ListNode(5);
+        n4.next = n5;
+        ListNode n6 = new ListNode(6);
+        n5.next = n6;
+
+        MiddleoftheLinkedListFor30Days m = new MiddleoftheLinkedListFor30Days();
+
+        ListNode middle = m.middleNode(n1);
+
+        System.out.println(middle.getVal());
+    }
+}
