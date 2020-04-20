25cf05c171b59a1b10d1fea978c8fb527d4d4f1d
张新罡 <zhangxingang@maoyan.com>
2020-04-20 20:19:29 +0800

    update

---
+++ src/com.leetcode.zh3ng.solutions/April30Days/ConstructBinarySearchTreefromPreorderTraversal.java
@@ -0,0 +1,63 @@
+package com.leetcode.zh3ng.solutions.April30Days;
+
+import model.TreeNode;
+
+/**
+ * created by zhangxingang on 2020/04/20
+ *
+ * Return the root node of a binary search tree that matches the given preorder traversal.
+ *
+ * (Recall that a binary search tree is a binary tree where for every node, any descendant of node.left has a value < node.val,
+ * and any descendant of node.right has a value > node.val.
+ * Also recall that a preorder traversal displays the value of the node first,
+ * then traverses node.left, then traverses node.right.)
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: [8,5,1,7,10,12]
+ * Output: [8,5,10,1,7,null,12]
+ *
+ *
+ *
+ * Note:
+ *
+ * 1 <= preorder.length <= 100
+ * The values of preorder are distinct.
+ */
+public class ConstructBinarySearchTreefromPreorderTraversal {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        TreeNode root = new TreeNode(preorder[0]);
+        for (int i = 1;i<preorder.length;i++){
+            addTree(root,preorder,i);
+        }
+        return root;
+    }
+
+    public void addTree(TreeNode now, int[] preorder, int index){
+        if (now.val > preorder[index]){
+            if (now.left == null){
+                now.left = new TreeNode(preorder[index]);
+                return;
+            }else{
+                addTree(now.left,preorder,index);
+            }
+        }
+        if (now.val < preorder[index]){
+            if (now.right == null){
+                now.right = new TreeNode(preorder[index]);
+            }else{
+                addTree(now.right,preorder,index);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ConstructBinarySearchTreefromPreorderTraversal c = new ConstructBinarySearchTreefromPreorderTraversal();
+
+        TreeNode root = c.bstFromPreorder(new int[]{8,5,1,7,10,12});
+
+        System.out.println(root.val);
+    }
+}
