d41e409e8443607411927c4d3490a8f85f2c6b48
张新罡 <zhangxingang@maoyan.com>
2020-04-21 18:25:00 +0800

    update

--- src/model/BinaryMatrix.java
+++ src/model/BinaryMatrix.java
@@ -2,8 +2,18 @@ package model;
 
 import java.util.List;
 
-/*
+/**
  * created by zhangxingang on 2020/04/21
+ * a Matrix
+ *
+ * |--------- m ------|
+ * --------------------
+ * |   |  x x x x .. x|
+ * |   |  x x x x .. x|
+ * n   |  x x x x .. x|
+ * |   |  x x x x .. x|
+ * |   |  x x x x .. x|
+ * --------------------
  */
 public interface BinaryMatrix {
     public int get(int x, int y);
