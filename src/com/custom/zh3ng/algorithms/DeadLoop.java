6707babaf3e7ab978c65287496d000ef65baa256
zhangxingang <zhangxingang@maoyan.com>
2021-02-08 16:46:30 +0800

    add a solution 31

---
+++ src/com/custom/zh3ng/algorithms/DeadLoop.java
@@ -0,0 +1,11 @@
+package com.custom.zh3ng.algorithms;
+
+/**
+ * @author zhangxingang
+ * @created on 2020-09-30
+ */
+public class DeadLoop {
+    public static void main(String[] args) {
+
+    }
+}
